d974dae0c769005cb47e816f821721933247d16d
Sourav Pal <143326384+Souravpal08@users.noreply.github.com>
2024-01-18 20:47:58 +0530

    Add files via upload

---
+++ Calculating_Age.py
@@ -0,0 +1,7 @@
+Myage=int (input("Enter your age:-"))
+years_left=90-Myage
+days_left=years_left*365
+months_left=years_left*12
+weeks_left=years_left*7
+
+print(f"You have{days_left}days, {months_left}months, &{weeks_left}weeks")
